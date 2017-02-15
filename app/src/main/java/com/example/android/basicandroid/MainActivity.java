60c26ade863befdbeec25a60d9867bb8ddd6d23b
Your Name <giyoon21c@gmail.com>
2017-02-15 08:06:36 -0800

    basic android functions

--- app/src/main/java/com/example/android/basicandroid/MainActivity.java
+++ app/src/main/java/com/example/android/basicandroid/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "DemoInitialApp";
+
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,33 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "This is a magic log msg!");
                 Toast.makeText(getApplicationContext(), "It's magic!", Toast.LENGTH_SHORT)
                         .show();
+                TextView msg = (TextView) findViewById(R.id.textView);
+                count++;
+
+                // using strings.xml to define
+                String message = getString(R.string.how_about_now);
+                msg.setText(message + count);
+            }
+        });
+
+        // setup a launch botton from scratch!
+        setUpLaunchButton();
+    }
+
+    /**
+     *  setting up setOnClickListener
+     */
+    private void setUpLaunchButton() {
+        Button launchButton = (Button) findViewById(R.id.launch_button);
+        launchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Launch!", Toast.LENGTH_SHORT)
+                        .show();
             }
         });
 
 
     }
+
 }
